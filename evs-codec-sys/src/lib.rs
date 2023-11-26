6d16e740dad7dbe113ef3cf99c4783381aa3da97
Jack Liar <9817146+jackliar@user.noreply.gitee.com>
2023-11-26 18:47:53 +0800

    evs use workspace deps; evs add macos aarch64 target ffi codes

--- evs-codec-sys/src/lib.rs
+++ evs-codec-sys/src/lib.rs
@@ -1,15 +1,18 @@
 #[cfg(feature = "gen")]
 include!(concat!(env!("OUT_DIR"), "/evs_codec_sys.rs"));
 
-#[cfg(all(not(feature = "gen"), target_os = "macos"))]
+#[cfg(all(not(feature = "gen"), target_os = "macos", target_arch = "x86_64"))]
 include!("macos_x86_64.rs");
 
-#[cfg(feature = "floating-point")]
-macro_rules! EVS {
-    ($field_name:ident) => {
-        $field_name
-    };
-}
+#[cfg(all(not(feature = "gen"), target_os = "macos", target_arch = "aarch64"))]
+include!("macos_aarch64.rs");
+
+// #[cfg(feature = "floating-point")]
+// macro_rules! EVS {
+//     ($field_name:ident) => {
+//         $field_name
+//     };
+// }
 
 // impl Decoder_State {
 //     pub fn reset_on_mime(&mut self) {
