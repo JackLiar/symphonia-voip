eac85c8694124cbad45ca7bc328438ce7bf72503
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2024-05-16 22:36:45 +0800

    add codec detector

---
+++ codec-detector/src/lib.rs
@@ -0,0 +1,223 @@
+//! Original algorithm: Fast RTP Detection and Codecs Classification in Internet Traffic(2014)
+
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufReader, Seek};
+use std::path::Path;
+use std::sync::Arc;
+
+use anyhow::Result;
+use fraction::Fraction;
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
+
+pub mod rtp;
+use rtp::{parse_rtp_event, PayloadType, RtpPacket};
+
+#[derive(Clone, Debug, Deserialize, Serialize, Eq, Hash, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Codec {
+    pub name: Arc<String>,
+    pub sample_rate: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub channels: Option<u8>,
+    pub bit_rate: Option<u32>,
+}
+
+impl Codec {
+    pub fn new(name: String, sample_rate: u32, channels: Option<u8>) -> Self {
+        Self {
+            name: Arc::new(name),
+            sample_rate,
+            channels,
+            bit_rate: None,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CodecFeature {
+    payload_size: Option<u16>,
+    delta_time: u32,
+    #[serde(skip_deserializing)]
+    ratio: Option<Fraction>,
+}
+
+impl CodecFeature {
+    pub fn new(payload_size: Option<u16>, delta_time: u32) -> Self {
+        Self {
+            payload_size,
+            delta_time,
+            ratio: payload_size.map(|ps| Fraction::new(delta_time, ps)),
+        }
+    }
+
+    fn set_radio(&mut self) {
+        self.ratio = self
+            .payload_size
+            .map(|ps| Fraction::new(self.delta_time, ps));
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct CodecDetector {
+    pt_pkt_stat: HashMap<PayloadType, u64>,
+    codec_stat: HashMap<PayloadType, HashMap<Codec, u64>>,
+    features: IndexMap<Codec, Vec<CodecFeature>>,
+    last_seq: HashMap<u32, u16>,
+    last_ts: HashMap<u32, u32>,
+    pub max_uniq_payload_size_num: usize,
+    payload_size_stat: HashMap<PayloadType, HashSet<usize>>,
+}
+
+impl CodecDetector {
+    pub fn new() -> Self {
+        CodecDetector {
+            max_uniq_payload_size_num: 3,
+            ..Default::default()
+        }
+    }
+}
+
+impl CodecDetector {
+    pub fn add_feature(&mut self, codec: Codec, ft: CodecFeature) {
+        match self.features.get_mut(&codec) {
+            None => {
+                self.features.insert(codec, vec![ft]);
+            }
+            Some(fts) => fts.push(ft),
+        }
+    }
+
+    fn add_payload_len<P: RtpPacket>(&mut self, pkt: &P) {
+        let payload_len = pkt.payload().len();
+        match self.payload_size_stat.get_mut(&pkt.payload_type()) {
+            None => {
+                let mut lens = HashSet::new();
+                lens.insert(payload_len);
+                self.payload_size_stat.insert(pkt.payload_type(), lens);
+            }
+            Some(lens) => {
+                if !lens.contains(&payload_len) {
+                    lens.insert(payload_len);
+                }
+            }
+        };
+    }
+
+    fn is_dynamic_len<P: RtpPacket>(&mut self, pkt: &P) -> bool {
+        match self.payload_size_stat.get(&pkt.payload_type()) {
+            None => unreachable!("payload_size_stat always have incoming RTP payload type"),
+            Some(lens) => lens.len() > self.max_uniq_payload_size_num,
+        }
+    }
+
+    fn last_seq<P: RtpPacket>(&self, pkt: &P) -> u16 {
+        match self.last_seq.get(&pkt.ssrc()) {
+            Some(s) => *s,
+            None => 0,
+        }
+    }
+
+    fn last_ts<P: RtpPacket>(&self, pkt: &P) -> u32 {
+        match self.last_ts.get(&pkt.ssrc()) {
+            Some(ts) => *ts,
+            None => 0,
+        }
+    }
+
+    pub fn on_pkt<P: RtpPacket>(&mut self, pkt: &P) {
+        if parse_rtp_event(pkt.payload()).is_ok() || !pkt.payload_type().is_dynamic() {
+            // Filter out all RTP event pkts and non dynamic codec pkts
+            return;
+        }
+
+        self.add_payload_len(pkt);
+        match self.pt_pkt_stat.get_mut(&pkt.payload_type()) {
+            None => {
+                self.pt_pkt_stat.insert(pkt.payload_type(), 1);
+            }
+            Some(cnt) => *cnt += 1,
+        };
+
+        if (pkt.seq() - self.last_seq(pkt)) != 1 {
+            self.last_seq.insert(pkt.ssrc(), pkt.seq());
+            self.last_ts.insert(pkt.ssrc(), pkt.ts());
+            return;
+        }
+
+        let delta_time = pkt.ts().wrapping_sub(self.last_ts(pkt));
+        self.last_seq.insert(pkt.ssrc(), pkt.seq());
+        self.last_ts.insert(pkt.ssrc(), pkt.ts());
+
+        let payload_len = if self.is_dynamic_len(pkt) {
+            None
+        } else {
+            Some(pkt.payload().len() as u16)
+        };
+        let ft = CodecFeature::new(payload_len, delta_time);
+
+        for (codec, fts) in &self.features {
+            for f in fts {
+                let ft_match = match ft.payload_size {
+                    Some(_) => f.ratio == ft.ratio,
+                    None => f.delta_time == ft.delta_time,
+                };
+                if ft_match {
+                    match self.codec_stat.get_mut(&pkt.payload_type()) {
+                        None => {
+                            let mut stat = HashMap::new();
+                            stat.insert(codec.clone(), 1);
+                            self.codec_stat.insert(pkt.payload_type(), stat);
+                        }
+                        Some(stat) => {
+                            if let Some(stat) = stat.get_mut(codec) {
+                                *stat += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn on_pkts<'a, I, P: RtpPacket + 'a>(&mut self, pkts: I)
+    where
+        I: IntoIterator<Item = &'a P>,
+    {
+        for pkt in pkts {
+            self.on_pkt(pkt)
+        }
+    }
+
+    pub fn get_result(&self) -> HashMap<PayloadType, Codec> {
+        let mut result = HashMap::new();
+        for (pt, stat) in &self.codec_stat {
+            let tot_cnt = self.pt_pkt_stat.get(pt).unwrap_or(&0);
+            for (codec, cnt) in stat {
+                if *cnt > (tot_cnt * 618 / 1000) {
+                    result.insert(*pt, codec.clone());
+                    break;
+                }
+            }
+        }
+        result
+    }
+
+    pub fn pts(&self) -> Vec<PayloadType> {
+        self.pt_pkt_stat.keys().cloned().collect()
+    }
+
+    pub fn get_features_from_yaml(&mut self, fpath: &Path) -> Result<()> {
+        let mut file = BufReader::new(File::open(fpath)?);
+        let codecs: Vec<Codec> = serde_yaml::from_reader(&mut file)?;
+        file.rewind()?;
+        let features: Vec<CodecFeature> = serde_yaml::from_reader(&mut file)?;
+        for (codec, mut ft) in codecs.iter().zip(features) {
+            ft.set_radio();
+            self.add_feature(codec.clone(), ft);
+        }
+        Ok(())
+    }
+}
---
+++ codec-detector/src/rtp.rs
@@ -0,0 +1,542 @@
+use std::fmt::Display;
+use std::ops::{Add, Sub};
+
+use anyhow::{anyhow, bail, Result};
+use combine::error::UnexpectedParse;
+use combine::parser::byte::num::be_u16;
+use combine::parser::byte::{byte, bytes};
+use combine::parser::range::take;
+use combine::parser::repeat::skip_many;
+use combine::{look_ahead, many1, Parser};
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+use serde::Serialize;
+
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[repr(transparent)]
+pub struct SeqNum(pub u16);
+
+impl Add for SeqNum {
+    type Output = u16;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let (seq, _) = self.0.overflowing_add(rhs.0);
+        seq
+    }
+}
+
+impl Sub for SeqNum {
+    type Output = u16;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let (seq, _) = self.0.overflowing_sub(rhs.0);
+        seq
+    }
+}
+
+impl From<u16> for SeqNum {
+    fn from(x: u16) -> Self {
+        Self(x)
+    }
+}
+
+impl From<SeqNum> for u16 {
+    fn from(x: SeqNum) -> Self {
+        x.0
+    }
+}
+
+/// RTP payload type, range from 0~127
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+#[repr(u8)]
+pub enum PayloadType {
+    #[default]
+    PCMU = 0,
+    CELP = 1,
+    G721 = 2,
+    GSM = 3,
+    G723 = 4,
+    DVI4_8000 = 5,
+    DVI4_16000 = 6,
+    LPC = 7,
+    PCMA = 8,
+    G722 = 9,
+    L16_44100_2 = 10,
+    L16_44100_1 = 11,
+    QCELP = 12,
+    CN = 13,
+    MPA = 14,
+    G728 = 15,
+    DVI4_11025 = 16,
+    DVI4_22050 = 17,
+    G729 = 18,
+    CELB = 25,
+    JPEG = 26,
+    NV = 28,
+    H261 = 31,
+    MPV = 32,
+    MP2T = 33,
+    H263 = 34,
+    Reserved(u8),
+    Dynamic(u8),
+    Unassigned(u8),
+}
+
+impl PayloadType {
+    pub fn to_u8(self) -> u8 {
+        match self {
+            Self::PCMU => 0,
+            Self::CELP => 1,
+            Self::G721 => 2,
+            Self::GSM => 3,
+            Self::G723 => 4,
+            Self::DVI4_8000 => 5,
+            Self::DVI4_16000 => 6,
+            Self::LPC => 7,
+            Self::PCMA => 8,
+            Self::G722 => 9,
+            Self::L16_44100_2 => 10,
+            Self::L16_44100_1 => 11,
+            Self::QCELP => 12,
+            Self::CN => 13,
+            Self::MPA => 14,
+            Self::G728 => 15,
+            Self::DVI4_11025 => 16,
+            Self::DVI4_22050 => 17,
+            Self::G729 => 18,
+            Self::CELB => 25,
+            Self::JPEG => 26,
+            Self::NV => 28,
+            Self::H261 => 31,
+            Self::MPV => 32,
+            Self::MP2T => 33,
+            Self::H263 => 34,
+            Self::Reserved(t) | Self::Dynamic(t) | Self::Unassigned(t) => t,
+        }
+    }
+
+    pub fn from_u8(t: u8) -> Self {
+        match t & 0x7f {
+            0 => Self::PCMU,
+            3 => Self::GSM,
+            4 => Self::G723,
+            5 => Self::DVI4_8000,
+            6 => Self::DVI4_16000,
+            7 => Self::LPC,
+            8 => Self::PCMA,
+            9 => Self::G722,
+            10 => Self::L16_44100_2,
+            11 => Self::L16_44100_1,
+            12 => Self::QCELP,
+            13 => Self::CN,
+            14 => Self::MPA,
+            15 => Self::G728,
+            16 => Self::DVI4_11025,
+            17 => Self::DVI4_22050,
+            18 => Self::G729,
+            25 => Self::CELB,
+            26 => Self::JPEG,
+            28 => Self::NV,
+            31 => Self::H261,
+            32 => Self::MPV,
+            33 => Self::MP2T,
+            34 => Self::H263,
+            t if t == 1 || t == 2 || t == 19 => Self::Reserved(t),
+            t if (72..=76).contains(&t) => Self::Reserved(t),
+            t if (96..=127).contains(&t) => Self::Dynamic(t),
+            t => Self::Unassigned(t),
+        }
+    }
+
+    pub fn is_dynamic(self) -> bool {
+        matches!(self, Self::Dynamic(_))
+    }
+}
+
+impl Display for PayloadType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::PCMU => "PCMU",
+            Self::CELP => "CELP",
+            Self::G721 => "G721",
+            Self::GSM => "GSM",
+            Self::G723 => "G723",
+            Self::DVI4_8000 => "DVI4",
+            Self::DVI4_16000 => "DVI4",
+            Self::LPC => "LPC",
+            Self::PCMA => "PCMA",
+            Self::G722 => "G722",
+            Self::L16_44100_2 => "L16",
+            Self::L16_44100_1 => "L16",
+            Self::QCELP => "QCELP",
+            Self::CN => "CN",
+            Self::MPA => "MPA",
+            Self::G728 => "G728",
+            Self::DVI4_11025 => "DVI4",
+            Self::DVI4_22050 => "DVI4",
+            Self::G729 => "G729",
+            Self::CELB => "CelB",
+            Self::JPEG => "JPEG",
+            Self::NV => "NV",
+            Self::H261 => "H261",
+            Self::MPV => "MPV",
+            Self::MP2T => "MP2T",
+            Self::H263 => "H263",
+            Self::Dynamic(t) => return format!("DYNAMIC-{}", t).fmt(f),
+            Self::Reserved(t) => return format!("RESERVED-{}", t).fmt(f),
+            Self::Unassigned(t) => return format!("UNASSIGNED-{}", t).fmt(f),
+        }
+        .fmt(f)
+    }
+}
+
+impl Serialize for PayloadType {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Extension<'a> {
+    pub id: u8,
+    pub value: &'a [u8],
+}
+
+pub trait RtpPacket {
+    fn raw(&self) -> &[u8];
+    fn version(&self) -> u8 {
+        (self.raw()[0] & 0b1100_0000) >> 6
+    }
+
+    fn padding(&self) -> bool {
+        (self.raw()[0] & 0b0010_0000) == 0b0010_0000
+    }
+
+    fn extension(&self) -> bool {
+        (self.raw()[0] & 0b0001_0000) == 0b0001_0000
+    }
+
+    fn csi_cnt(&self) -> usize {
+        (self.raw()[0] & 0x0f) as usize
+    }
+
+    fn marked(&self) -> bool {
+        (self.raw()[1] & 0x80) == 0x80
+    }
+
+    fn payload_type(&self) -> PayloadType {
+        PayloadType::from_u8(self.raw()[1])
+    }
+
+    fn seq(&self) -> u16 {
+        match <&[u8; 2]>::try_from(&self.raw()[2..4]) {
+            Ok(seq) => u16::from_be_bytes(*seq),
+            Err(_) => unreachable!(),
+        }
+    }
+
+    fn ts(&self) -> u32 {
+        match <&[u8; 4]>::try_from(&self.raw()[4..8]) {
+            Ok(seq) => u32::from_be_bytes(*seq),
+            Err(_) => unreachable!(),
+        }
+    }
+
+    fn ssrc(&self) -> u32 {
+        match <&[u8; 4]>::try_from(&self.raw()[8..12]) {
+            Ok(seq) => u32::from_be_bytes(*seq),
+            Err(_) => unreachable!(),
+        }
+    }
+
+    fn payload(&self) -> &[u8] {
+        let mut buf = if !self.extension() {
+            &self.raw()[12..]
+        } else {
+            let mut offset = 12 + 2;
+            let ext_len = match <&[u8; 2]>::try_from(&self.raw()[offset..offset + 2]) {
+                Ok(seq) => u16::from_be_bytes(*seq) as usize,
+                Err(_) => unreachable!(),
+            } * 4;
+            offset += ext_len;
+            &self.raw()[offset..]
+        };
+
+        if self.padding() {
+            if let Some(padding_len) = buf.last() {
+                buf = &buf[0..(buf.len() - (*padding_len as usize))];
+            }
+        }
+
+        buf
+    }
+
+    fn get_extensions(&self) -> Result<Option<Vec<()>>> {
+        if !self.extension() {
+            return Ok(None);
+        }
+
+        match look_ahead(bytes(b"\xbe\xde")).parse(&self.raw()[12..]) {
+            Ok((_, rem)) => {
+                // One byte header extensions
+                let (exts, _) = take(2)
+                    .and(be_u16())
+                    .then(|(_magic, len)| {
+                        take(len as usize * 4).and_then(|a: &[u8]| {
+                            if !a.is_empty() {
+                                let ext_parser = take(1)
+                                    .map(|b: &[u8]| (b[0] & 0xf0, (b[0] & 0x0f) + 1))
+                                    .then(|(id, len)| take(len as usize + 1).map(move |r| (id, r)))
+                                    .skip(skip_many(byte(0x00)))
+                                    .map(|(id, value)| Extension { id, value });
+                                many1::<Vec<_>, _, _>(ext_parser)
+                                    .parse(a)
+                                    .map(|(exts, _)| exts)
+                            } else {
+                                Ok(vec![])
+                            }
+                        })
+                    })
+                    .parse(rem)?;
+                exts
+            }
+            Err(UnexpectedParse::Unexpected) => {
+                // Two byte header extensions
+                let (exts, _) = take(2)
+                    .and(be_u16())
+                    .then(|(_magic, len)| {
+                        take(len as usize * 4).and_then(|a: &[u8]| {
+                            if !a.is_empty() {
+                                let ext_parser = take(1)
+                                    .and(take(1))
+                                    .map(|(id, len): (&[u8], &[u8])| (id[0], len[0] as usize))
+                                    .then(|(id, len)| take(len).map(move |r| (id, r)))
+                                    .skip(skip_many(byte(0x00)))
+                                    .map(|(id, value)| Extension { id, value });
+                                many1::<Vec<_>, _, _>(ext_parser)
+                                    .parse(a)
+                                    .map(|(exts, _)| exts)
+                            } else {
+                                Ok(vec![])
+                            }
+                        })
+                    })
+                    .parse(&self.raw()[12..])?;
+                exts
+            }
+            Err(UnexpectedParse::Eoi) => unreachable!(),
+        };
+        todo!()
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct RawRtpPacket<'a> {
+    raw: &'a [u8],
+}
+
+impl<'a> RtpPacket for RawRtpPacket<'a> {
+    fn raw(&self) -> &[u8] {
+        self.raw
+    }
+}
+
+impl<'a> RawRtpPacket<'a> {
+    pub fn new(raw: &'a [u8]) -> Self {
+        Self { raw }
+    }
+}
+
+pub fn parse_rtp(data: &[u8]) -> Result<RawRtpPacket> {
+    let (_hdr, mut rem) = take(12).parse(data)?;
+
+    let pkt = RawRtpPacket { raw: data };
+    if pkt.extension() {
+        let (_exts, r) = take(2)
+            .and(be_u16())
+            .then(|(_magic, len)| take(len as usize * 4))
+            .parse(rem)?;
+        rem = r;
+    }
+
+    if pkt.padding() {
+        let len = match rem.last() {
+            None => bail!("Invalid RTP Packet: no payload avaliable"),
+            Some(l) => *l as usize,
+        };
+        if len >= rem.len() {
+            bail!("Invalid RTP Packet: padding is longer than payload len");
+        } else {
+            rem = &rem[0..rem.len() - 1 - len];
+        }
+    }
+
+    if rem.is_empty() {
+        bail!("Invalid RTP Packet: no payload avaliable");
+    }
+
+    Ok(pkt)
+}
+
+/// Detect whether a packet is not a RTP packet
+pub fn detect_not_rtp(data: &[u8], ssrcs: &[u32]) -> bool {
+    if data.is_empty() {
+        return true;
+    }
+
+    if data[0] < 0x80 || data[0] > 0xbf {
+        return true;
+    }
+
+    if data.len() >= 8 && data[4..8] == [0x21, 0x12, 0xa4, 0x42] {
+        // skip STUN packets
+        return true;
+    }
+
+    if data.len() >= 8 {
+        // skip RTCP packets
+        let ssrc = ((data[4] as u32) << 24)
+            | ((data[5] as u32) << 16)
+            | ((data[6] as u32) << 8)
+            | (data[7] as u32);
+        if ssrcs.contains(&ssrc) {
+            return true;
+        }
+    }
+
+    false
+}
+
+#[derive(Clone, Copy, Debug, Default, Eq, FromPrimitive, Hash, PartialEq)]
+#[repr(u8)]
+pub enum EventCode {
+    #[default]
+    DTMF0 = 0,
+    DTMF1 = 1,
+    DTMF2 = 2,
+    DTMF3 = 3,
+    DTMF4 = 4,
+    DTMF5 = 5,
+    DTMF6 = 6,
+    DTMF7 = 7,
+    DTMF8 = 8,
+    DTMF9 = 9,
+    Star = 10,
+    Pound = 11,
+    A = 12,
+    B = 13,
+    C = 14,
+    D = 15,
+    Flash = 16,
+}
+
+impl Display for EventCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::DTMF0 => "DTMF 0",
+            Self::DTMF1 => "DTMF 1",
+            Self::DTMF2 => "DTMF 2",
+            Self::DTMF3 => "DTMF 3",
+            Self::DTMF4 => "DTMF 4",
+            Self::DTMF5 => "DTMF 5",
+            Self::DTMF6 => "DTMF 6",
+            Self::DTMF7 => "DTMF 7",
+            Self::DTMF8 => "DTMF 8",
+            Self::DTMF9 => "DTMF 9",
+            Self::Star => "DTMF *",
+            Self::Pound => "DTMF #",
+            Self::A => "DTMF A",
+            Self::B => "DTMF B",
+            Self::C => "DTMF C",
+            Self::D => "DTMF D",
+            Self::Flash => "Flash",
+        }
+        .fmt(f)
+    }
+}
+
+impl Serialize for EventCode {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct RtpEvent {
+    pub event_id: EventCode,
+    pub flags: u8,
+    pub duration: u16,
+}
+
+impl RtpEvent {
+    pub fn is_end_of_event(&self) -> bool {
+        self.flags & 0b10000000 == 0b10000000
+    }
+}
+
+/// Parse RTP event ID heuristically
+pub fn parse_rtp_event(data: &[u8]) -> Result<RtpEvent> {
+    let (((event_id, flags), duration), rem) = take(1).and(take(1)).and(be_u16()).parse(data)?;
+    let event_id = EventCode::from_u8(event_id[0]).ok_or_else(|| anyhow!("Invalid RTP EventID"))?;
+    if !rem.is_empty() {
+        bail!("Payload type is not RTP Event");
+    }
+    Ok(RtpEvent {
+        event_id,
+        flags: flags[0],
+        duration,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_rtp() -> Result<()> {
+        let data: &[u8] = &[
+            0x80, 0x7f, 0x00, 0x02, 0x08, 0x37, 0x76, 0x60, 0x00, 0x84, 0x1a, 0xa8, 0x8b, 0x73,
+            0x6f, 0xf5, 0x58, 0x4a, 0xc0, 0x90, 0x44, 0xc4, 0x50, 0x16, 0x03, 0xd8, 0x07, 0xfe,
+            0x19, 0x2b, 0x80, 0x28, 0x02, 0x00, 0x80, 0x00, 0x16, 0x70, 0x90, 0x5c, 0x69, 0xdc,
+            0xf0, 0xa9, 0x5c,
+        ];
+        let rtp = parse_rtp(data)?;
+        assert!(!rtp.marked());
+        assert_eq!(rtp.payload_type(), PayloadType::Dynamic(127));
+        assert_eq!(rtp.seq(), 0x0002);
+        assert_eq!(rtp.ts(), 0x08377660);
+        assert_eq!(rtp.ssrc(), 0x00841aa8);
+        assert_eq!(rtp.payload().len(), 33);
+        Ok(())
+    }
+
+    #[test]
+    fn test_seq_num() -> Result<()> {
+        let seq1 = SeqNum(1);
+        let seq2 = SeqNum(2);
+        assert_eq!(seq2 - seq1, 1);
+
+        let seq1 = SeqNum(2);
+        let seq2 = SeqNum(2);
+        assert_eq!(seq2 - seq1, 0);
+
+        let seq1 = SeqNum(3);
+        let seq2 = SeqNum(2);
+        assert_eq!(seq2 - seq1, 65535);
+
+        let seq1 = SeqNum(65535);
+        let seq2 = SeqNum(0);
+        assert_eq!(seq2 - seq1, 1);
+
+        let seq1 = SeqNum(0);
+        let seq2 = SeqNum(65535);
+        assert_eq!(seq2 - seq1, 65535);
+        Ok(())
+    }
+}
