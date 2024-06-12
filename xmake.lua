168f00664fa26d1f67de7a5a4d31fbfa8b696d5f
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2024-06-12 22:32:08 +0800

    fix g7221 macos build issue

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,12 @@
 set_policy("package.install_locally", true)
 
 add_requires("opencore-amr", {system = false})
+add_requires("libtool", {system = false})
 add_requires("g7221", {system = false})
 
 package("g7221")
     add_urls("https://github.com/freeswitch/libg7221.git")
+    add_deps("libtool")
     on_install("linux", "macosx", "android", "iphoneos", "bsd", "cross", "mingw", function (package)
         local configs = {}
         table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
